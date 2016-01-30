6139ad95dd564cfbd39791cbeb9d85cba8ad0c6c
Tero Paavolainen <Tero Paavolainen>
2016-01-30 17:58:04 +0200

    worldobject# Please enter the commit message for your changes. Lines starting

---
+++ src/WorldObject.js
@@ -0,0 +1,16 @@
+
+
+function WorldObject(id){
+	this.id = id;
+	this.x = 0;
+	this.y = 0;
+
+}
+
+WorldObject.prototype.setPosition = function(x,y){
+	this.x = x;
+	this.y = y;
+}
+
+
+module.exports.WorldObject = WorldObject;
